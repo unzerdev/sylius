a3438a219de1e1977d1c8c6615f20b92afe5f124
Branko Janjic <branko.janjic@logeecom.com>
2024-10-08 22:23:37 +0200

    Core

--- src/Controller/ConfigurationController.php
+++ src/Controller/ConfigurationController.php
@@ -4,13 +4,47 @@ declare(strict_types=1);
 
 namespace SyliusUnzerPlugin\Controller;
 
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
+use Sylius\Component\Core\Factory\PaymentMethodFactoryInterface;
+use Sylius\Component\Core\Repository\PaymentMethodRepositoryInterface;
 final class ConfigurationController extends AbstractController
 {
+
+
+    /**
+     * @param PaymentMethodFactoryInterface $paymentMethodFactory
+     * @param PaymentMethodRepositoryInterface $paymentMethodRepository
+     * @param ChannelRepositoryInterface $channelRepository
+     */
+    public function __construct(  private PaymentMethodFactoryInterface $paymentMethodFactory,
+        private PaymentMethodRepositoryInterface $paymentMethodRepository,
+        private ChannelRepositoryInterface $channelRepository)
+    {
+    }
+
+    /**
+     *
+     * @return Response
+     */
     public function configAction(): Response
     {
         return $this->render('@SyliusUnzerPlugin/config.html.twig');
+    }
+
+    public function enableAction(): Response
+    {
+        $paymentMethod = $this->paymentMethodRepository->findOneBy(['code' => 'unzer_payment']);
+
+        if ($paymentMethod == null) {
+            // add payment method in database.
+            $paymentMethod = $this->paymentMethodFactory->createWithGateway('unzer_payment');
+            $paymentMethod->getGatewayConfig()?->setGatewayName('unzer_payment');
+            $paymentMethod->setCode('unzer_payment');
+            $paymentMethod->setName('UNZER');
+            $this->paymentMethodRepository->add($paymentMethod);
+        }
+        return $this->redirectToRoute('unzer_admin_config');
     }
 }
---
+++ src/Twig/Extension/UnzerEnabled.php
@@ -0,0 +1,49 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace SyliusUnzerPlugin\Twig\Extension;
+
+use Sylius\Component\Core\Model\PaymentMethodInterface;
+use Sylius\Component\Core\Repository\PaymentMethodRepositoryInterface;
+use SyliusMolliePlugin\Checker\ApplePay\ApplePayEnabledCheckerInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class UnzerEnabled extends AbstractExtension
+{
+    /**
+     * @var PaymentMethodRepositoryInterface<PaymentMethodInterface>
+     */
+    private PaymentMethodRepositoryInterface $paymentMethodRepository;
+
+    /**
+     * @param PaymentMethodRepositoryInterface<PaymentMethodInterface> $paymentMethodRepository
+     */
+    public function __construct(PaymentMethodRepositoryInterface $paymentMethodRepository)
+    {
+        $this->paymentMethodRepository = $paymentMethodRepository;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction(
+                'is_unzer_enabled',
+                [$this, 'isEnabled'],
+                ['is_safe' => ['html']]
+            ),
+        ];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        $paymentMethod = $this->paymentMethodRepository->findOneBy(['code' => 'unzer_payment']);
+
+        return $paymentMethod !== null;
+    }
+}
