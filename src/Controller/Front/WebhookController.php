5090ab267ca2a409defe0a4847d5175c8cc06491
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2024-10-25 10:34:47 +0200

    Implement webhook controller

--- src/Controller/Front/WebhookController.php
+++ src/Controller/Front/WebhookController.php
@@ -7,6 +7,13 @@ namespace SyliusUnzerPlugin\Controller\Front;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Unzer\Core\BusinessLogic\Domain\Checkout\Exceptions\CurrencyMismatchException;
+use Unzer\Core\BusinessLogic\Domain\Checkout\Exceptions\InvalidCurrencyCode;
+use Unzer\Core\BusinessLogic\Domain\Connection\Exceptions\ConnectionSettingsNotFoundException;
+use Unzer\Core\BusinessLogic\Domain\TransactionHistory\Exceptions\TransactionHistoryNotFoundException;
+use Unzer\Core\BusinessLogic\WebhookAPI\Handler\Request\WebhookHandleRequest;
+use Unzer\Core\BusinessLogic\WebhookAPI\WebhookAPI;
+use UnzerSDK\Exceptions\UnzerApiException;
 
 /**
  * Class WebhookController.
@@ -16,13 +23,26 @@ use Symfony\Component\HttpFoundation\Response;
 class WebhookController extends AbstractController
 {
     /**
-     * @param string|null $name
+     * @param Request $request
      *
      * @return Response
+     *
+     * @throws UnzerApiException
+     * @throws CurrencyMismatchException
+     * @throws InvalidCurrencyCode
+     * @throws ConnectionSettingsNotFoundException
+     * @throws TransactionHistoryNotFoundException
      */
     public function webhookAction(Request $request): Response
     {
+        $storeId = $request->get('storeId');
+        $storeId = is_string($storeId) ? $storeId : '';
+        $response = WebhookAPI::get()->webhookHandle($storeId)->handle(new WebhookHandleRequest($request->getContent()));
 
-        return $this->json(['']);
+        if (!$response->isSuccessful()) {
+            return $this->json($response->toArray(), 400);
+        }
+
+        return $this->json([]);
     }
 }
