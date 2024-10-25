fc06361454c512d890489f12fc447a21e71652bc
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2024-10-25 11:13:32 +0200

    Implement default status map

--- src/Services/Integration/PaymentStatusMapService.php
+++ src/Services/Integration/PaymentStatusMapService.php
@@ -2,8 +2,15 @@
 
 namespace SyliusUnzerPlugin\Services\Integration;
 
+use Sylius\Component\Payment\Model\PaymentInterface;
 use Unzer\Core\BusinessLogic\Domain\Integration\PaymentStatusMap\PaymentStatusMapServiceInterface;
+use Unzer\Core\BusinessLogic\Domain\PaymentStatusMap\Enums\PaymentStatus;
 
+/**
+ * Class PaymentStatusMapService.
+ *
+ * @package SyliusUnzerPlugin\Services\Integration
+ */
 class PaymentStatusMapService implements PaymentStatusMapServiceInterface
 {
     /**
@@ -11,6 +18,22 @@ class PaymentStatusMapService implements PaymentStatusMapServiceInterface
      */
     public function getDefaultPaymentStatusMap(): array
     {
-        return [];
+        return self::defaultStatusMap();
+    }
+
+    /**
+     * @return array
+     */
+    public static function defaultStatusMap(): array
+    {
+        return [
+            PaymentStatus::PAID => PaymentInterface::STATE_COMPLETED,
+            PaymentStatus::UNPAID => PaymentInterface::STATE_NEW,
+            PaymentStatus::FULL_REFUND => PaymentInterface::STATE_REFUNDED,
+            PaymentStatus::CANCELLED => PaymentInterface::STATE_CANCELLED,
+            PaymentStatus::COLLECTION => PaymentInterface::STATE_PROCESSING,
+            PaymentStatus::PARTIAL_REFUND => PaymentInterface::STATE_REFUNDED,
+            PaymentStatus::DECLINED => PaymentInterface::STATE_FAILED
+        ];
     }
 }
