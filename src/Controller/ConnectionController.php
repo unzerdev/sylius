c03198f0003c72b07b31ea637e6292fea4d74f51
Branko Janjic <branko.janjic@logeecom.com>
2024-10-26 00:48:08 +0200

    Checkout and admin actions

--- src/Controller/ConnectionController.php
+++ src/Controller/ConnectionController.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace SyliusUnzerPlugin\Controller;
 
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\PaymentMethodInterface;
+use Sylius\Component\Core\Repository\PaymentMethodRepositoryInterface;
+use SyliusUnzerPlugin\Util\StaticHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,25 +26,54 @@ final class ConnectionController extends AbstractController
 
     /**
      * @param Request $request
-     *
+     * @param ChannelRepositoryInterface<ChannelInterface> $channelRepository
+     * @param PaymentMethodRepositoryInterface<PaymentMethodInterface> $paymentMethodRepository
      * @return Response
      *
-     * @throws UnzerApiException
      * @throws ConnectionSettingsNotFoundException
      * @throws InvalidKeypairException
      * @throws InvalidModeException
      * @throws PrivateKeyInvalidException
      * @throws PublicKeyInvalidException
+     * @throws UnzerApiException
      */
-    public function connectAction(Request $request): Response
+    public function connectAction(
+        Request $request,
+        ChannelRepositoryInterface $channelRepository,
+        PaymentMethodRepositoryInterface $paymentMethodRepository
+    ): Response
     {
+        /** @var string $storeId */
+        $storeId = $request->get('storeId', '');
+        /** @var string $environment */
+        $environment = $request->get('environment', '');
+        /** @var string $publicKey */
+        $publicKey = $request->get('publicKey', '');
+        /** @var string $privateKey */
+        $privateKey = $request->get('privateKey', '');
+
+        /** @var ?ChannelInterface $channel */
+        $channel = $channelRepository->find((int)$storeId);
+        /** @var ?PaymentMethodInterface $paymentMethod */
+        $paymentMethod = $paymentMethodRepository->findOneBy(['code' => StaticHelper::UNZER_PAYMENT_METHOD_GATEWAY]);
+        if (($paymentMethod === null) || ($channel === null)) {
+            return $this->json(['error' => 'Payment method or channel not found'], 404);
+        }
+
+        if ($paymentMethod->hasChannel($channel)) {
+            return $this->json(['error' => 'Channel already connected'], 400);
+        }
+        $paymentMethod->addChannel($channel);
+
+        $paymentMethodRepository->add($paymentMethod);
+
         $response = AdminAPI::get()->connection(
-            $request->get('storeId')
+            $storeId
         )->connect(
             new ConnectionRequest(
-                $request->get('environment') ?? '',
-                $request->get('publicKey') ?? '',
-                $request->get('privateKey') ?? ''
+                $environment,
+                $publicKey,
+                $privateKey
             )
         );
 
