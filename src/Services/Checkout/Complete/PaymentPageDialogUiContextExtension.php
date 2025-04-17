dcf2db636650da9d9b0dc0bb3a6cd488260a3952
Boljanovic <aleksandar.boljanovic@logeecom.com>
2025-04-17 09:53:38 +0200

    Refactor payment selection flow

--- src/Services/Checkout/Complete/PaymentPageDialogUiContextExtension.php
+++ src/Services/Checkout/Complete/PaymentPageDialogUiContextExtension.php
@@ -6,6 +6,8 @@ namespace SyliusUnzerPlugin\Services\Checkout\Complete;
 
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Order\Context\CartContextInterface;
+use Sylius\Component\Core\Repository\OrderRepositoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
@@ -15,21 +17,31 @@ use Twig\TwigFunction;
  */
 class PaymentPageDialogUiContextExtension extends AbstractExtension
 {
-    public function __construct(private UrlGeneratorInterface $router, private CartContextInterface $cartContext)
-    {
+    public function __construct(
+        private UrlGeneratorInterface $router,
+        private CartContextInterface $cartContext,
+        private RequestStack $requestStack,
+        private OrderRepositoryInterface $orderRepository,
+    ) {
     }
 
     public function unzerProviderData(): array
     {
         $templateContext = [];
+
         /** @var OrderInterface $order */
         $order = $this->cartContext->getCart();
+        $orderId = $order->getId();
+
+        if (null === $orderId) {
+            $orderId = $this->getOrderIdFromRequest();
+        }
 
         $paymentDetails = [];
         $unzerPaymentType = '';
 
         $payment = $order->getLastPayment();
-        if (null !== $payment && $payment->getMethod()?->getCode() === 'unzer_payment' ) {
+        if (null !== $payment && $payment->getMethod()?->getCode() === 'unzer_payment') {
             $paymentDetails = $payment->getDetails();
         }
 
@@ -38,7 +50,10 @@ class PaymentPageDialogUiContextExtension extends AbstractExtension
             $unzerPaymentType = $paymentDetails['unzer']['payment_type'] ?? '';;
         }
 
-        $templateContext['unzer_payment_page_url'] = $this->router->generate('unzer_paypage_create', ['orderId' => $order->getId()]);
+        $templateContext['unzer_payment_page_url'] = $this->router->generate(
+            'unzer_paypage_create',
+            ['orderId' => $orderId]
+        );
         $templateContext['unzer_payment_error'] = $this->router->generate('unzer_payment_error');
         $templateContext['unzer_payment_page_type'] = $unzerPaymentType;
 
@@ -50,5 +65,23 @@ class PaymentPageDialogUiContextExtension extends AbstractExtension
         return [
             new TwigFunction('unzer_provider_data', [$this, 'unzerProviderData']),
         ];
+    }
+
+    /**
+     * @return ?int
+     */
+    private function getOrderIdFromRequest(): ?int
+    {
+
+        $request = $this->requestStack->getCurrentRequest();
+        $tokenValue = $request->get('tokenValue') ?? null;
+
+        if (null !== $tokenValue) {
+            $order = $this->orderRepository->findOneByTokenValue($tokenValue);
+
+            return $order->getId();
+        }
+
+        return null;
     }
 }
